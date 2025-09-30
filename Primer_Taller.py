1bf95a1f6c236a07fa18d45e0d5df725e5d5a23c
evhernandezt <evhernandezt@unal.edu.co>
2025-09-29 23:35:36 -0500

    second

--- Primer_Taller.py
+++ Primer_Taller.py
@@ -1,4 +1,27 @@
-import "numpy" as np
-import "matplotlib.pyplot" as plt
+# Para graficar
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+import warnings
+warnings.filterwarnings("ignore")
 
- print("Hola, bienvenido al primer taller de Python");
+mpl.rc('axes', labelsize=14)
+mpl.rc('xtick', labelsize=12)
+mpl.rc('ytick', labelsize=12)
+
+A = 1 # amplitud de la señal
+T = 5 # segundos de gráfica
+Fs = 100 # frecuencia de muestreo definida
+tv = np.arange(-T/2,T/2,1/Fs) #vector de tiempo generado
+Nm = len(tv) #numero de muestras requeridas
+
+#se crea la señal A y -A concatenando
+xo = np.concatenate((A*np.ones(int(Nm/2)), -A*np.ones(int(Nm/2))), axis=0)
+#graficar señal original
+plt.plot(tv,xo,'r',linewidth=4)
+plt.grid()
+plt.xlabel("t[s]",fontsize = 14)
+plt.ylabel("x(t)",fontsize = 14)
+plt.show()
+
+
